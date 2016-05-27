4023fe8a6af8eecb66261483b35674dba67211aa
Sylvain as root <sylvain@opensource-expert.com>
2016-05-27 15:43:16 +0200

    auto doc tools for removing all users.

--- tools/remove_all_users.sql
+++ tools/remove_all_users.sql
@@ -1,6 +1,10 @@
--- cleanup every users
--- Usage: mysql mysql < remove_all_users.sql
--- requires root privileges
+-- cleanup every users (except those mentioned)
+-- Usage:
+--   mysql mysql < remove_all_users.sql
+--   #or
+--   salt-cp 'minion' remove_all_users.sql /root
+--   salt 'minion' cmd.run 'mysql mysql < remove_all_users.sql'
+-- requires root privileges!
 --
 SET sql_log_bin=0;
 DELETE FROM user         WHERE user NOT IN('root', 'debian-sys-maint');
