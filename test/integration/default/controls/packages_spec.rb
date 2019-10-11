fca3b0431fa77b9c1102e367756d3408f40aa6ec
Imran Iqbal <iqbalmy@hotmail.com>
2019-10-11 20:49:09 +0100

    fix(rubocop): add fixes using `rubocop --safe-auto-correct`

--- test/integration/default/controls/packages_spec.rb
+++ test/integration/default/controls/packages_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # Override by OS
 package_name = 'mariadb-server'
-if os[:name] == 'suse' or os[:name] == 'opensuse'
+if (os[:name] == 'suse') || (os[:name] == 'opensuse')
   package_name = 'mariadb'
-elsif os[:name] == 'debian' and os[:release].start_with?('8')
+elsif (os[:name] == 'debian') && os[:release].start_with?('8')
   package_name = 'mysql-server'
 end
 
